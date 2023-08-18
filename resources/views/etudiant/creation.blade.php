327edd7a1522841bdf1cb0a9501382bfc8de4b68
marine-devweb <marine.devweb@gmail.com>
2023-08-18 14:09:13 +0200

    Màj du formulaire de la création d'étudiant

--- resources/views/etudiant/creation.blade.php
+++ resources/views/etudiant/creation.blade.php
@@ -3,25 +3,24 @@
 
 <!-- content est une convention de nommage -->
 @section('content')
-<div class="container">
+<div class="container mt-5">
     <h1>Création d'un nouvel étudiant</h1>
 
     <form action="{{ route('etudiants.store') }}" method="POST">
     @csrf
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <div class="mb-3 mt-5">
+            <label for="nom" class="form-label">Nom</label>
+            <input type="text" class="form-control" id="nom_id" aria-describedby="emailHelp">
         </div>
         <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
+            <label for="prenom" class="form-label">Prénom</label>
+            <input type="text" class="form-control" id="prenom_id">
         </div>
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        <div class="mb-3">
+            <label for="classe" class="form-label">Classe</label>
+            <input type="text" class="form-control" id="classe_id">
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">Envoyer</button>
     </form>
 </div>
 @endsection
