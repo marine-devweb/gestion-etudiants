88cd1d8cf2104983cfa06177e23bfc1f8c94115e
marine-devweb <marine.devweb@gmail.com>
2023-08-22 15:32:33 +0200

    Ajout de l'attribut disabled sur le formulaire de consultation

--- resources/views/etudiant/voir.blade.php
+++ resources/views/etudiant/voir.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<form class="mx-5 no-edit bold" action="{{ route('etudiants.show', $etudiant->id) }}" method="GET">
+<form class="mx-5 bold" action="{{ route('etudiants.show', $etudiant->id) }}" method="GET">
     @csrf
     <div class="mb-3 mt-5">
         <label for="Nom" class="form-label">Nom</label>
-        <input type="text" class="form-control" id="nom_id" name="nom" value="{{ $etudiant->nom }}">
+        <input type="text" class="form-control" id="nom_id" name="nom" value="{{ $etudiant->nom }}" disabled>
     </div>
     <div class="mb-3">
         <label for="prenom" class="form-label">Prénom</label>
-        <input type="text" class="form-control" name="prenom" value="{{ $etudiant->prenom }}" id="prenom_id">
+        <input type="text" class="form-control" name="prenom" value="{{ $etudiant->prenom }}" id="prenom_id" disabled>
     </div>
     <div class="mb-3">
         <label for="classe" class="form-label">Classe</label>
-        <input type="text" class="form-control" name="classe" value="{{ $etudiant->classe }}" id="classe_id">
+        <input type="text" class="form-control" name="classe" value="{{ $etudiant->classe }}" id="classe_id" disabled>
     </div>
 </form>
 @endsection
